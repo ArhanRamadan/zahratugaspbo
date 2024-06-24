e59c892b4a7a7e7f6340ce2266f082beded86147
ArhanRamadan <arhanramadhan243@gmail.com>
2024-06-24 13:22:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">    
+    <title>Zahra kun</title>
+</head>
+<body>
+    <header>
+        <h1>Pemrograman Berorientasi Objek</h1>
+    </header>
+    <nav>
+        <a href="index.php" class="active">Home</a>
+        <a href="class_method/classMethod.php">Kelas Method Objek</a>
+        <a href="encaptulation/index.php">Encapsulation</a>
+        <a href="inheritance/index.php">Inheritance</a>
+        <a href="polymorphism/index.php">Polymorphism</a>
+        <a href="magic_method/index.php">Magic Method</a>
+        <a href="abstraksi/index.php">Abstraksi</a>
+        <a href="overriding/index.php">Overriding</a>
+        <a href="overloading/index.php">Overloading</a>
+    </nav>
+    <main>
+    <div class="container">
+        <section class="box">
+            <!-- <img class="background" src="germany.svg" alt=""> -->
+            <h2>Home Page</h2>
+            <p class="caption">Silahkan pilih menu di atas</p>
+            </div>
+        </section>
+    </div>
+    <!-- <audio src="Mars-uin.mp3" autoplay loop></audio> -->
+    </main>
+    <footer>&copy; 236250084</footer>
+</body>
+</html>
